e0f26a70a688a0620d68e1e808b81769735eabb0
king david levi <125862163+kingdavidlevi@users.noreply.github.com>
2023-12-30 15:12:50 -0700

    updated

--- src/Account.js
+++ src/Account.js
@@ -13,7 +13,7 @@ return(
     <div className="mb-36">
 
 
- <div className= {`${mobileFixed ? "flex justify-between px-8 w-full z-10 fixed top-0   lg:hidden h-14 place-items-center bg-gray-300 " :  "flex px-8 justify-between w-full z-10    lg:hidden h-14 place-items-center bg-gray-300 " } `} >
+ <div className=  "flex justify-between px-8 w-full z-10 fixed top-0   lg:hidden h-14 place-items-center bg-gray-300 "  >
 <div><FaBars className="text-lg "/></div>
 
 <div><p>Welcome</p></div>
@@ -155,8 +155,8 @@ return(
 <div className=" mt-4 grid place-items-center ">
 
     <div className="flex justify-end gap-4 lg:w-256 xl:w-255">
-    <button className="md:w-53 h-12 text-lg font-medium hover:bg-slate-200 rounded-md" type="button" >Cancel</button>
-    <button className="text-white text-lg font-normal md:w-53 h-12 rounded-md bg-red-600" type="button" >Save Changes</button>
+    <button className="md:w-53 bg-gray-300 h-12 text-lg font-medium hover:bg-slate-200 rounded-md" type="button" >Cancel</button>
+    <button className="text-white text-lg font-normal md:w-53 h-12 rounded-md bg-red-600 hover:bg-red-500" type="button" >Save Changes</button>
     </div>
 </div>
 
@@ -173,7 +173,7 @@ return(
 
 
 
-<div className={`${mobileFixed ? "shadow-2xl lg:hidden block mt-30 bg-white xl:w-310 lg:w-280 md:w-260 md:h-260 w-90 pt-4  md:pt-10" : "shadow-2xl pt-4 w-90 lg:hidden block mt-16 bg-white xl:w-310 lg:w-280 md:w-260 md:h-260  md:pt-10" }`}>
+<div className= "shadow-2xl lg:hidden block mt-30 bg-white  md:w-260 md:h-260 sm:w-100 w-86 pb-4 pt-4  md:pt-10" >
 
 
     <div className="grid place-items-center ">
@@ -192,13 +192,13 @@ return(
 
 <div className="xl:w-82 md:w-70 lg:w-75 md:h-20 mt-6">
  <label  className="font-medium">First Name</label> <br/>
- <input type="text" className=" text-gray-400  bg-gray-100 md:h-10 rounded-md w-70 pl-4 outline-none mt-1 md:w-70 lg:w-75 xl:w-82" />
+ <input type="text" className=" text-gray-400  bg-gray-100 md:h-10 sm:w-96 rounded-md w-70 pl-4 outline-none mt-1 md:w-70 lg:w-75 xl:w-82" />
 </div>
 
 
 <div className="xl:w-82 lg:w-75 md:h-20 mt-6">
  <label  className= "font-medium">Last Name</label> <br/>
- <input type="text" className=" md:w-70 text-gray-400 w-70  bg-gray-100 rounded-md md:h-10 outline-none pl-4 lg:w-75 mt-1 xl:w-82" />
+ <input type="text" className=" md:w-70 text-gray-400 w-70 sm:w-96 bg-gray-100 rounded-md md:h-10 outline-none pl-4 lg:w-75 mt-1 xl:w-82" />
 </div>
 
 
@@ -209,13 +209,13 @@ return(
 
 <div className="xl:w-82 lg:w-75 md:h-20 mt-8">
  <label  className="font-medium">Email</label> <br/>
- <input type="email" className=" md:w-70 lg:w-75 w-70 text-gray-400  bg-gray-100 md:h-10 rounded-md pl-4 outline-none mt-1 xl:w-82" />
+ <input type="email" className=" md:w-70 lg:w-75 w-70 sm:w-96 text-gray-400  bg-gray-100 md:h-10 rounded-md pl-4 outline-none mt-1 xl:w-82" />
 </div>
 
 
 <div className="xl:w-82 lg:w-75 md:h-20 mt-8">
  <label   className="font-medium">Address</label> <br/>
- <input type="text" className="text-gray-400 w-70 lg:w-75 md:w-70 bg-gray-100 rounded-md md:h-10 outline-none pl-4 mt-1 xl:w-82" />
+ <input type="text" className="text-gray-400 w-70 sm:w-96 lg:w-75 md:w-70 bg-gray-100 rounded-md md:h-10 outline-none pl-4 mt-1 xl:w-82" />
 </div>
 
 
@@ -232,9 +232,9 @@ return(
 <label   className="font-medium">Password Changes</label> <br/>
 
 <div className="w-full grid place-items-center mt-1.5 md:mt-1">
-<input className="xl:w-255 md:w-200 lg:w-256 w-70 rounded-md h-10  text-gray-400 outline-none  bg-gray-100 pl-4" type="password" placeholder="Current Password"/> <br/>
-<input className="xl:w-255 md:w-200 lg:w-256 w-70 rounded-md h-10 text-gray-400 outline-none bg-gray-100 pl-4" type="password" placeholder="New Pasword"/> <br/>
-<input className="xl:w-255 md:w-200 lg:w-256 w-70 rounded-md h-10 text-gray-400 outline-none bg-gray-100 pl-4" type="password" placeholder="Confirm New Password"/> <br/>
+<input className="xl:w-255 md:w-200 lg:w-256 w-70 sm:w-96 rounded-md h-10  text-gray-400 outline-none  bg-gray-100 pl-4" type="password" placeholder="Current Password"/> <br/>
+<input className="xl:w-255 md:w-200 lg:w-256 w-70 sm:w-96 rounded-md h-10 text-gray-400 outline-none bg-gray-100 pl-4" type="password" placeholder="New Pasword"/> <br/>
+<input className="xl:w-255 md:w-200 lg:w-256 w-70 sm:w-96 rounded-md h-10 text-gray-400 outline-none bg-gray-100 pl-4" type="password" placeholder="Confirm New Password"/> <br/>
 
 </div>
 
@@ -247,9 +247,9 @@ return(
 
 <div className=" mt-4 grid place-items-center ">
 
-    <div className="flex justify-end gap-4 lg:w-256 xl:w-255">
-    <button className="md:w-53 h-12 text-lg font-medium hover:bg-slate-200 rounded-md" type="button" >Cancel</button>
-    <button className="text-white text-lg font-normal md:w-53 h-12 rounded-md bg-red-600" type="button" >Save Changes</button>
+    <div className="flex justify-around sm:justify-end gap-4 sm:w-96 lg:w-256 xl:w-255">
+    <button className="md:w-53 h-12 text-lg w-40 bg-gray-300 font-medium hover:bg-slate-200 rounded-md" type="button" >Cancel</button>
+    <button className="text-white text-lg  w-40  font-normal md:w-53 h-12 rounded-md bg-red-600" type="button" >Save Changes</button>
     </div>
 </div>
 
