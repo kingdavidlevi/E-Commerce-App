a90efc0d09eaef8532cf01733323af43656f23c6
king david levi <125862163+kingdavidlevi@users.noreply.github.com>
2024-01-09 12:18:26 -0700

    updated

--- src/Search.js
+++ src/Search.js
@@ -19,11 +19,12 @@ useEffect(() => {
   return(
 
     <div>
-        <div className=" w-full items-center  grid place-items-center h-18 bg-white shadow-md shadow-gray-300 ">
-     <header className="top-0 lg:w-340 md:w-270 xl:w-400 relative w-full  grid place-items-center  ">
-        <input type="text" ref={inputRef} placeholder="Search products, brands and categories" className="w-72 sm:w-82 md:pl-12  pr-6 md:border-r md:border  md:pr-0 pt-1 pb-1 placeholder:text-lg text-xl border-r-2 outline-none lg:pt-2 lg:pb-2 lg:w-150 xl:w-260 md:w-98 md:pt-1 md:pb-1   border-gray-300" />
-        <img src={search} className="absolute xl:left-80 md:left-40 lg:left-64 right-6" />
-        <div className="absolute left-6 lg:left-12" onClick={navigateBack}><FaArrowLeft className="text-xl"/></div>
+        <div className=" w-full items-center  grid place-items-center md:h-18 h-16 bg-white shadow-md shadow-gray-300 ">
+     <header className="top-0 lg:w-340 flex md:w-270 xl:w-400 md:grid items-center justify-around relative w-full place-items-center    ">
+     <div className="absolute left-6 lg:left-12 md:hidden block" onClick={navigateBack}><FaArrowLeft className="text-xl"/></div>
+      <div> <input type="text" ref={inputRef} placeholder="Search products, brands and categories" className="w-60 sm:w-96 md:pl-12 ml-14 md:ml-0 text-lg pr-6 md:border-r md:border  md:pr-0 pt-1 pb-1 placeholder:text-sm  border-r-2 outline-none lg:pt-2 lg:pb-2 lg:w-150 xl:w-260 md:w-98 md:pt-1 md:pb-1   border-gray-300" /> </div> 
+      <div>  <img src={search} className="absolute right-6 xl:left-80 bottom-2 md:left-40 lg:left-64 " /></div>
+        <div className="absolute left-6 lg:left-12 hidden md:block" onClick={navigateBack}><FaArrowLeft className="text-xl"/></div>
         <button className="bg-orange-400 absolute hidden md:block lg:px-3 xl:px-4 md:px-1 h-10 xl:ml-1 mt-1 md:right-14 lg:right-28 text-white  font-medium shadow-lg rounded-md">SEARCH</button>
      </header>
 
