4b2c99665f29e3f24c9f619eec6d9363a4d5b4fb
king david levi <125862163+kingdavidlevi@users.noreply.github.com>
2024-01-18 13:58:45 -0700

    upgraded

--- src/Footer.js
+++ src/Footer.js
@@ -2,73 +2,71 @@ import React from "react";
 import gitHub from './images/logo-github.png'
 import linkedin from './images/logo-linkedin.svg'
 import twitter from './images/logo-twitter.svg'
-
+import { FaInstagram, FaFacebook,FaTwitter } from "react-icons/fa";
 
 function Footer (){
     return(
 
        <div className=" inset-x-0 bottom-0 bg-black text-white pt-8 pb-10 block">
+      
 
-        <div className="md:flex  md:justify-around  ">
+      <div className="mt-4 grid mx-10 border-b-r border-white place-items-center">
+       <ul>
+        <li  className="text-xl  text-center font-bold text-white">
+          Exclusive
+        </li>
+        <li  className="text-lg mt-6 text-center font-medium text-white"> 
+          Gift & decoration store
+        </li>
+        <li  className="text-lg mt-6 text-center font-medium text-white">
+          Shopping made easy
+        </li>
+        < li className="text-lg  mt-6 text-center  text-white">
+          Contact
+        </li>
+        <li className="text-lg mb-6 mt-6 text-center f text-white">
+         Cart
+        </li>
+         
+       </ul>
 
-      <div>
-        <h3 className="text-lg font-sm">Exclusive</h3>
-        <h4 className="mt-4 text-sm">Subscribe</h4>
-        <p className="mt-4 text-sm">Get 10% off your first order</p>
 
       </div>
-
-
-    <div>
-        <h3 className="text-lg font-sm" >Support</h3>
-        <p className="mt-4 mb-4 text-sm">Block 324 Texas,USA,New York<br/>City Avenue </p>
-        <a href="https://levikingdavid4040@gmail.com" className="underline text-blue-300  ">levikingdavid4040@gmail.com</a>
-        <p className="mt-4 text-sm">+234-906-777-4541</p>
-    </div>
+     
 
 
 
-<div>
-<h3 className="text-lg font-sm">Account</h3>
-<p  className="mt-4 text-sm">Cart</p>
-<p className="mt-4 text-sm">Wishlist</p>
-<p className="mt-4 text-sm">Shop</p>
-</div>
+        <div className="grid place-items-center mt-6" >
+        
+        <ul className=" flex gap-5 ">
+          <li>
+            <FaInstagram className="text-lg" />
+          </li>
+          <li>
+            <FaFacebook className="text-lg"/>
+          </li>
+          <li>
+            <FaTwitter className="text-lg"/>
+          </li>
+        </ul>
+
+        <ul className="flex gap-8 mt-6" >
+
+          <li className="font-medium text-lg text-white">privacy Policy</li>
+
+          <li className="font-medium text-lg text-white">Terms of Use </li>
+        </ul>
 
 
-
-
-<div>
-<h3 className="text-lg ">Quick Link</h3>
-<p className="mt-4 text-sm">Privacy Policy</p>
-<p className="mt-4 text-sm">Terms Of Use</p>
-<p className="mt-4 text-sm">FAQ</p>
- 
-</div>
-
-
-<div>
-    <h3 className="text-lg font-sm">Contact Me</h3>
-   <div>
-    <p className="mt-4 text-sm">
-   <a href="https://github.com/kingdavidlevi" target="_blank"> Github</a>
-   </p>
-
-   <p className="mt-4 text-sm">
-   <a href="#" target="_blank">LinkedIn</a>
-   </p>
-
-   <p className="mt-4 text-sm">
-     <a href=" https://twitter.com/Code_King_" target="_blank">Twitter</a>
-     </p>        
-   </div>
-    
-</div>
-
+        <ul className="mt-6">
+          <li className="font-medium text-white">
+            Copyright 2023 Exclusive. All rights reserved.
+          </li>
+        </ul>
 
         </div>
-
-
+       
+      
 
         </div>
     )
